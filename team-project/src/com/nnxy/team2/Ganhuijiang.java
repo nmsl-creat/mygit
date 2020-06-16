91de28609246a97f59083ed713976f6066e550b9
huangbangyi2019 <1094476088@qq.com>
2020-06-16 11:38:18 +0800

    哇咔咔

--- team-project/src/com/nnxy/team2/Ganhuijiang.java
+++ team-project/src/com/nnxy/team2/Ganhuijiang.java
@@ -2,4 +2,16 @@ package com.nnxy.team2;
 
 public class Ganhuijiang {
 
+	private int id;
+	private String name;
+	private String password;
+	private int sex;
+	
+	public Ganhuijiang(int id,String name,String password,int sex) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.sex = sex;
+	}
 }
