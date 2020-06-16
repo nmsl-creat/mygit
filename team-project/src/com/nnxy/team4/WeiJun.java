342460951f7b2b4a061902322d4b701588a5c473
huangbangyi2019 <1094476088@qq.com>
2020-06-16 11:27:01 +0800

    2020/6/16  11：26

--- team-project/src/com/nnxy/team4/WeiJun.java
+++ team-project/src/com/nnxy/team4/WeiJun.java
@@ -1,10 +1,13 @@
 package com.nnxy.team4;
 
 public class WeiJun {
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("20170217122  Îº¿¡");
 	}
 
 }
